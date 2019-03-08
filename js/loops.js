f9d3ba1a0a250307e8cfc391d97cc9ede8fc125a
ab.gupta1 <abhishake.gupta@geminisolutions.in>
2019-03-08 09:53:00 +0530

    Add loop js file

---
+++ js/loops.js
@@ -0,0 +1,22 @@
+console.log('>>>>>>>> Loops');
+// Repeat a piece of code when a condition is true.
+
+var arr = ['John', 'smith', 'bob', 'Abhi'];
+
+for (var i = 0; i<arr.length; i++ ) {
+    console.log(arr[i]);
+}
+
+i = 0;
+while (i <= 10) {
+    i++;
+    console.log(i);
+}
+
+
+var j = 0
+do{
+    console.log(arr[j]);
+    j++
+
+}while(j<arr.length);
